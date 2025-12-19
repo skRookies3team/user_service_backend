18eed56f26ed7c106352c72868f3f0e2bb4130ff
손민정 <ecolead@naver.com>
2025-12-19 14:24:38 +0900

    ã보안규칙변경

--- src/main/java/com/example/petlog/security/SecurityConfig.java
+++ src/main/java/com/example/petlog/security/SecurityConfig.java
@@ -59,15 +59,6 @@ public class SecurityConfig {
                                 "/v3/api-docs/**"
                         ).permitAll()
 
-                        // 정적 이미지 리소스 허용
-                        .requestMatchers("/products/images/**").permitAll()
-                        //인가 설정: 모든 경로에 대한 접근을 특정 ip 주소에서 온 요청으로만 제한한다.
-                        //hasIpAddress(): 요청을 보낸 클라이언트의 ip 주소가 지정된 주소와 일치하는지 확인한다.
-                        //(127.0.0.1은 localhost, ::1은 IPv6의 localhost를 의미한다.
-                        .requestMatchers("/**").access(
-                                new WebExpressionAuthorizationManager(
-                                        "hasIpAddress('127.0.0.1') or hasIpAddress('::1') or " +
-                                                "hasIpAddress('10.90.26.89') or hasIpAddress('::1')")) // host pc ip address
                         //그 외 모든 API 요청은 JWT 인증 필수
                         .anyRequest().authenticated())
                         //Http Basic 인증을 security 필터 체인에 추가한다.
@@ -75,7 +66,7 @@ public class SecurityConfig {
                         //클라이언트가 api 요청을 보낼때 authorization 헤더에
                         //[Base64인코딩된 아이디: 비밀번호] 형식으로 인증 정보를 담아 보내면
                         //Spring Security가 이를 자동으로 해석하고 인증을 시도한다.
-                        .httpBasic(Customizer.withDefaults());  // ← Basic 인증 추가
+                        //.httpBasic(Customizer.withDefaults());  // ← Basic 인증 추가
 
 
 
